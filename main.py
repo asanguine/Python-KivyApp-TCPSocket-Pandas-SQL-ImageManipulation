d4baae3ec9e0ee77b8047a7c739e325a158fb4a7
Asanguine <birkan.kurt@proton.me>
2023-12-10 17:54:12 +0100

    added timer countdown

--- main.py
+++ main.py
@@ -5,15 +5,64 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.button import Button
 from kivy.core.window import Window
 from kivy.uix.slider import Slider
+from kivy.clock import Clock
+from datetime import timedelta
+
+########################  TimerWindow  ##########################
 
 class TimerWindow(ModalView):
-    pass
+    def start_timer(self, duration):
+        main_screen = App.get_running_app().root
+        main_screen.start_timer(duration)
+
+########################  MainScreen  ############################
 
 class MainScreen(FloatLayout):
+
     def show_timer_window(self):
         timer_window = TimerWindow()
         timer_window.open()
 
+
+    def on_timer_dismiss(self, instance):
+        pass
+
+
+    def update_timer(self, instance, value):
+        hours, remainder = divmod(value * 60, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        time_str = f"{int(hours):02d}:{int(minutes):02d}:{int(seconds):02d}"
+
+        timer_label = self.ids.timer_label
+        timer_label.text = time_str
+
+
+    def start_timer(self, duration):
+        total_seconds = duration * 60
+
+        def update_callback(dt):
+            nonlocal total_seconds
+            if total_seconds > 0:
+                total_seconds -= 1
+                hours, remainder = divmod(total_seconds, 3600)
+                minutes, seconds = divmod(remainder, 60)
+                if (hours == 0):
+                    time_str = f"{int(minutes):02d}:{int(seconds):02d}"
+                else:             
+                    time_str = f"{int(hours):02d}:{int(minutes):02d}:{int(seconds):02d}"
+
+                timer_label = self.ids.timer_label
+                timer_label.text = time_str
+            else:
+                #when the timer reaches 0
+                Clock.unschedule(update_callback)
+        Clock.schedule_interval(update_callback, 1)
+
+
+
+
+######################################################
+######################################################
 
 class TogetherApp(App):
     def build(self):
