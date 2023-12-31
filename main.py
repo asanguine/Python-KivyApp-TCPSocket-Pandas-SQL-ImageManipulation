e91ea1454442513af38fb1bcbb4c9fff1d936da7
Asanguine <birkan.kurt@proton.me>
2023-12-31 16:19:31 +0100

    fixed window resize

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from main_screen import MainScreen
 from client import MyClient
 from state_manager import StateManager
 import logging
+from kivy.clock import Clock
 
 #  ______                        __    __                         __     
 # /\__  _\                      /\ \__/\ \                       /\ \    
@@ -28,13 +29,24 @@ class TogetherApp(App):
     def show_character_window(self):
         main_screen = self.root
         main_screen.show_character_window()
-    
+
+    def on_window_resize(self, instance, width, height):
+        pass
+
+    def update_aspect_ratio(self, dt):
+        aspect_ratio = 512 / 768
+        new_width = min(Window.width, Window.height * aspect_ratio)
+        new_height = min(Window.height, Window.width / aspect_ratio)
+        Window.size = (new_width, new_height)
+
     def build(self):
         state_manager = StateManager()
         client = MyClient(state_manager=state_manager)
         client.start()
         initialize_database()
         Window.size = (512, 768)
+        Window.bind(on_resize=self.on_window_resize)
+        Clock.schedule_interval(self.update_aspect_ratio, 1 / 60.0)
         return MainScreen()
 
 
